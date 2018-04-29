156571dcb441eaadcba4f50c3c607a3779417afc
AlexSaenen <alexander.saenen@epitech.eu>
2018-04-29 15:36:38 +0200

    Add more data in Task and display in TaskItem with onHover popup

--- src/components/TaskItem/index.js
+++ src/components/TaskItem/index.js
@@ -1,10 +1,11 @@
 // @flow
 
-import React from 'react';
+import React, { Component } from 'react';
 import Wrapper from './Wrapper';
 
 export type RawTask = {
   +name: string,
+  +notes: string,
   +day: string,
   +hour: number,
   +duration: number,
@@ -18,22 +19,59 @@ type Props = {
   +task: Task
 };
 
+type State = Props & {
+  showMore: boolean,
+};
+
 const startsAt: number = 7; // TODO: get this information better
 const lasts: number = 17;
 
-export default function TaskItem({ task }: Props) {
-  const startingPosition: number = task.hour - startsAt; // TODO: do checks
+class TaskItem extends Component<Props, State> {
+  state = {
+    ...this.state,
+    showMore: false,
+  };
 
-  return (
-    <Wrapper
-      style={{
-        position: 'absolute',
-        width: '100%',
-        top: `calc((${startingPosition} * 500px) / ${lasts})`,
-        height: `calc((${task.duration} * 500px) / ${lasts})`
-      }}
-    >
-      {task.name}
-    </Wrapper>
-  );
+  showMore = () => {
+    this.setState({
+      showMore: true,
+    });
+  }
+
+  showLess = () => {
+    this.setState({
+      showMore: false,
+    });
+  }
+
+  render() {
+    const { task } = this.props;
+    console.log(`TaskItem.render()#${task.id}`);
+    const { showMore } = this.state;
+    const startingPosition: number = task.hour - startsAt; // TODO: do checks
+
+    return (
+      <Wrapper
+        style={{
+          position: 'absolute',
+          width: '100%',
+          top: `calc((${startingPosition} * 500px) / ${lasts})`,
+          height: `calc((${task.duration} * 500px) / ${lasts})`
+        }}
+        onMouseEnter={this.showMore}
+        onMouseLeave={this.showLess}
+      >
+        <div>
+          {task.name}
+        </div>
+        {showMore &&
+          <div>
+            {task.notes}
+          </div>
+        }
+      </Wrapper>
+    );
+  }
 };
+
+export default TaskItem;
